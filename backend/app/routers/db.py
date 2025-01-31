f7d49c3aae8f1ee17220bebfa60ce455189a11bb
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-01-31 08:09:40 -0800

    Setup pydantic settings

--- backend/app/routers/db.py
+++ backend/app/routers/db.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from functools import lru_cache
+
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 
+from .. import config
+
 router = APIRouter()
+
+
+@lru_cache
+def get_settings():
+    return config.Settings()
 
 
 class TextRequest(BaseModel):
@@ -9,10 +18,12 @@ class TextRequest(BaseModel):
 
 
 @router.post("/upload")
-async def upload(body: TextRequest):
+async def upload(body: TextRequest, settings: config.Settings = Depends(get_settings)):
     return {"Message": body.text}
 
 
 @router.post("/retrieve")
-async def retrieve(body: TextRequest):
+async def retrieve(
+    body: TextRequest, settings: config.Settings = Depends(get_settings)
+):
     return {"Message": body.text}
