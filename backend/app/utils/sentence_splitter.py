2832523bd1c200895d659e68a015c267bba9444f
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-02-05 14:11:56 -0800

    Init NLTK

---
+++ backend/app/utils/sentence_splitter.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.tokenize import sent_tokenize
+
+
+class SentenceSplitter:
+    def __init__(self):
+        nltk.download("punkt")
+
+    def split(self, text):
+        return sent_tokenize(text)
