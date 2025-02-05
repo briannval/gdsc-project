0d0b0b2aebd739b125c3a899e84fce22731b0c6e
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-02-05 13:07:45 -0800

    Refactor to utils

--- backend/app/routers/db.py
+++ backend/app/routers/db.py
@@ -1,36 +1,9 @@
-from functools import lru_cache
-
 from fastapi import APIRouter
-from pinecone import Pinecone, ServerlessSpec
 from pydantic import BaseModel
-from sentence_transformers import SentenceTransformer
 
-from .. import config
+from ..utils.vector_store import delete_vector, retrieve_vectors, upload_vector
 
 router = APIRouter()
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-
-@lru_cache()
-def get_settings():
-    return config.Settings()
-
-
-# Pinecone settings
-pc = Pinecone(api_key=get_settings().pinecone_api_key)
-index_name = "gdsc-project"
-try:
-    pc.create_index(
-        name=index_name,
-        dimension=384,
-        metric="cosine",
-        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
-    )
-except Exception as e:
-    if "ALREADY_EXISTS" in str(e):
-        print(f"Index '{index_name}' already exists. Skipping creation.")
-index = pc.Index(index_name)
-namespace_name = "ubc-info"
 
 
 class TextRequest(BaseModel):
@@ -40,39 +13,19 @@ class TextRequest(BaseModel):
 @router.post("/upload")
 async def upload(body: TextRequest):
     txt = body.text
-    vector = model.encode(txt).tolist()
-    curr_vector_count = (index.describe_index_stats())["total_vector_count"]
-    new_vector_count = int(curr_vector_count) + 1
-    index.upsert(
-        vectors=[
-            {
-                "id": "Chunk-" + str(new_vector_count),
-                "values": vector,
-                "metadata": {"sentence": txt},
-            }
-        ],
-        namespace=namespace_name,
-    )
+    upload_vector(txt)
     return {"Message": "Success"}
 
 
 @router.post("/retrieve")
 async def retrieve(body: TextRequest):
     txt = body.text
-    query_vector = model.encode(txt).tolist()
-    results = index.query(
-        vector=query_vector,
-        top_k=5,
-        include_values=True,
-        include_metadata=True,
-        namespace=namespace_name,
-    )
-    match_sentences = [match["metadata"]["sentence"] for match in results.matches]
+    match_sentences = retrieve_vectors(txt)
     return {"Message": "Success", "Results": match_sentences}
 
 
 @router.delete("/delete")
 async def delete(body: TextRequest):
     vector_id = body.text
-    index.delete(ids=[vector_id], namespace=namespace_name)
+    delete_vector(vector_id)
     return {"Message": "Success"}
