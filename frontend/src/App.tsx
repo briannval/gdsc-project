8ab05e843b79c7edf5b771531b3861472d2b43e6
Davooood90 <davooood90@gmail.com>
2025-02-21 22:39:54 -0700

    Added pages to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,48 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
+
+// Main Page Sections
+import LandingPage from "./pages/LandingPage";
+import FeaturesPage from "./pages/FeaturesPage";
+import HIWPage from "./pages/HIWPage";
+import JoinPage from "./pages/JoinPage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
-  const [response, setResponse] = useState<string>("Hi there! How can I assist you?");
+  const [response, setResponse] = useState<string>(
+    "Hi there! How can I assist you?"
+  );
   const [value, setValue] = useState<string>("");
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(e.target.value);
 
-  const handleSubmit = async() => {
+  const handleSubmit = async () => {
     // the call to the api
-    const response = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    const response =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
     setResponse(response);
-  }
+  };
 
-  return(
-    <div className="container">
-      <div>
-        <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        ></input>
+  return (
+    <>
+      <LandingPage />
+      <FeaturesPage />
+      <HIWPage />
+      <JoinPage />
+      <AboutPage />
+      <div className="container">
+        <div>
+          <input type="text" value={value} onChange={onChange}></input>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>Submit</button>
+        </div>
+        <div>
+          <p>Chatbot: {response}</p>
+        </div>
       </div>
-      <div>
-        <button onClick={handleSubmit}>Submit</button>
-      </div>
-      <div>
-        <p>Chatbot: {response}</p>
-      </div>
-    </div>
+    </>
   );
 }
 
---
+++ frontend/src/pages/HIWPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HIWPage = () => {
+  return <div>HIWPage</div>;
+};
+
+export default HIWPage;
