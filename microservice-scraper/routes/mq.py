f3df20284343ca8b0bdef61285f4093e5e5ee9dc
brianadhitya <brianvalentinoadhitya@gmail.com>
2025-03-10 12:42:09 -0700

    .

--- microservice-scraper/routes/mq.py
+++ microservice-scraper/routes/mq.py
@@ -1,11 +1,12 @@
 from flask import Blueprint
+from utils.scraper import scrape_util
 
 mq_bp = Blueprint("mq_bp", __name__)
 
 
 @mq_bp.route("/scrape", methods=["POST"])
 def scrape():
-    pass
+    scrape_util("google.com")
 
 
 @mq_bp.route("/publish", methods=["POST"])
--- microservice-scraper/utils/scraper.py
+++ microservice-scraper/utils/scraper.py
@@ -1,2 +1,12 @@
-def scrape(url):
+import asyncio
+
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig
+
+
+async def scrape_util(url):
     print("SCRAPE " + str(url))
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+            url="https://www.example.com",
+        )
+        print(result.markdown[:300])
